c89c8b34bce0941d01a711a72d3b132794442350
Kongpop <2410511101007@live4.utcc.ac.th>
2026-04-01 14:41:17 +0700

    Fix priceCal.js

--- priceCal.js
+++ priceCal.js
@@ -1,9 +1,16 @@
 function priceCalculate() {
-  let ticknum = (document.getElementById("ticknum").value).trim();
-  let selectObj = document.getElementById("event");
-  let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let ticknum = parseInt(document.getElementById("ticknum").value.trim());
+
+  let eventPrices = {
+    1: 91.25,
+    2: 79.88,
+    3: 83.12,
+    4: 100
+  };
+
+  let eventId = document.getElementById("event").value;
+
+  let price = eventPrices[eventId];
+
+  return (price * ticknum).toFixed(2);
 }
-
-
